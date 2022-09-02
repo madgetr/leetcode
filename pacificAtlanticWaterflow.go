ebca79509ff35302289d73363e73d463f77aa190
madgetr <trevor.madge16@gmail.com>
2022-09-02 08:15:50 -0400

    Update pacificAtlanticWaterflow.go
    changed seen map from string to int column+(row*NumberOfColumns) to get cell

--- pacificAtlanticWaterflow.go
+++ pacificAtlanticWaterflow.go
@@ -1,33 +1,37 @@
+var ROWS int
+var COLS int
 func pacificAtlantic(heights [][]int) [][]int {
-    atl:=make(map[string][]int)
-    pac:=make(map[string][]int)
+    atl:=make(map[int][]int)
+    pac:=make(map[int][]int)
     ans:=[][]int{}
-    for i:=0;i<len(heights[0]);i++{
+    ROWS = len(heights)
+    COLS = len(heights[0])
+    for i:=0;i<COLS;i++{
         dfs(0,i,pac,heights[0][i],heights)
-        dfs(len(heights)-1,i,atl,heights[len(heights)-1][i],heights)
+        dfs(len(heights)-1,i,atl,heights[ROWS-1][i],heights)
     }
     
     for i:=0;i<len(heights);i++{
         dfs(i,0,pac,heights[i][0],heights)
-        dfs(i,len(heights[0])-1,atl,heights[i][len(heights[0])-1],heights)
+        dfs(i,COLS-1,atl,heights[i][COLS-1],heights)
     }
     for k,v:=range atl {
         _,ok:=pac[k]
-      if ok {
+        if ok {
             ans = append(ans,v)
         }
     }
     return ans
 }
-func dfs(r,c int, seen map[string][]int, prevHeight int, heights [][]int) {
-    if r<0 || c<0 || r>=len(heights) || c>=len(heights[r]) || prevHeight>heights[r][c]{
+func dfs(r,c int, seen map[int][]int, prevHeight int, heights [][]int) {
+    if r<0 || c<0 || r>=ROWS || c>=COLS || prevHeight>heights[r][c]{
         return 
     }
-    _,ok:=seen[string(r)+","+string(c)]
+    _,ok:=seen[c+(r*COLS)]
     if ok {
         return
     }
-    seen[string(r)+","+string(c)] = []int{r,c}
+    seen[c+(r*COLS)] = []int{r,c}
     dfs(r+1,c,seen,heights[r][c],heights)
     dfs(r-1,c,seen,heights[r][c],heights)
     dfs(r,c+1,seen,heights[r][c],heights)
